ab7d1858b34b481db2d2e8b07c40a89a3adffa94
Vasanth Prabhakaran <vasanthprabhakaran1999@gmail.com>
2020-12-02 00:45:28 +0530

    Post request completed

--- Client/src/components/Uploader.vue
+++ Client/src/components/Uploader.vue
@@ -1,13 +1,13 @@
 <template>
   <div class="container">
     <h1>{{ msg }}</h1>
-    <form class="form-group"> 
+    <form class="form-group">
       <div class="mt-3">
         <label for="customFile">
-           <button class="btn btn-success">
-            <input type="file" id="customFile" name="fileName" @change="handleFileChange" accept=".csv"/>
+          <button class="btn btn-success">
+            <input type="file" id="customFile" name="fileName" @change="handleFileChange" accept=".csv" />
             Choose File
-           </button> 
+          </button>
         </label>
       </div>
       <h4 v-if="file" class="mt-3">Selected file: {{ file }}</h4>
@@ -16,9 +16,8 @@
 </template>
 
 <script>
-
-import { ref } from 'vue'
-import {axios} from 'axios'
+import { ref } from "vue";
+import http from "../http-common";
 
 export default {
   name: "Uploader",
@@ -26,61 +25,56 @@ export default {
     msg: String,
   },
   setup() {
-    var file= ref('');
-     function handleFileChange(e) {
+    var file = ref("");
+    function handleFileChange(e) {
       this.file = e.target.files[0].name;
+      console.log(file);
       addRecords(file);
-          }
+    }
 
-function addRecords(file){
-let url = "https://localhost:5000/upload";
-      const request = new Request(
-        url,
-        {
-          method: "POST",
-          mode: "cors",
-          cache: "default",
-          body: file
-        }
-      );
-      const res = await fetch(request);
-      const data = await res.json();
-      this.data = data;
-}
+    function addRecords(localFile) {
+      let formData = new FormData();
 
-    return{
+      formData.append("file", localFile);
+
+      return http.post("/upload", formData, {
+        headers: {
+          "Content-Type": "multipart/form-data",
+        },
+      });
+    }
+
+    return {
       file,
       handleFileChange,
-    }
-  }
+    };
+  },
 };
 </script>
 
-<style >
-
+<style>
 h1 {
-  color:#35495e;
+  color: #35495e;
 }
 
 #customFile {
-   opacity: 0;
-   position: absolute;
-   width: 111px;
-   height: 38px;
-   z-index: 2;
-   margin: -7px -13px;
-   cursor: pointer;
-   overflow: hidden;
+  opacity: 0;
+  position: absolute;
+  width: 111px;
+  height: 38px;
+  z-index: 2;
+  margin: -7px -13px;
+  cursor: pointer;
+  overflow: hidden;
 }
-input{
-   cursor: pointer;
+input {
+  cursor: pointer;
 }
-.btn{
-    color: #35495e;
-    background-color:#1db36f;
+.btn {
+  color: #35495e;
+  background-color: #1db36f;
 }
 label {
   margin-top: 5px;
 }
-
 </style>
